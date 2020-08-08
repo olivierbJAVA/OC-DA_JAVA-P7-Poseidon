f8868ee1437ae8c6cc0dad7b87d17129c2eeecc3
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-08 10:24:48 +0200

    Work on domain entities

--- src/main/java/com/nnk/springboot/domain/BidList.java
+++ src/main/java/com/nnk/springboot/domain/BidList.java
@@ -12,6 +12,7 @@ import java.sql.Timestamp;
 @Table(name = "bidlist")
 public class BidList {
     // TODO: Map columns in data table BIDLIST with corresponding java fields
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer BidListId;
