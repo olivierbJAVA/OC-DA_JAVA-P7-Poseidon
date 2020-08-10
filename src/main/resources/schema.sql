0109260764d39521167bfab844c80bb02f8f2a2f
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-10 16:11:59 +0200

    Updating schema.sql database initialization files

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -100,3 +100,7 @@ CREATE TABLE Users (
 
 insert into Users(fullname, username, password, role) values("Administrator", "admin", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "ADMIN");
 insert into Users(fullname, username, password, role) values("User", "user", "$2a$10$pBV8ILO/s/nao4wVnGLrh.sa/rnr5pDpbeC4E.KNzQWoy8obFZdaa", "USER");
+
+insert into Rating(moodysRating, sandPRating, fitchRating, orderNumber) values ('AAA', 'AA+', 'AA-', 1);
+insert into Rating(moodysRating, sandPRating, fitchRating, orderNumber) values ('BBB', 'BB+', 'BB-', 2);
+insert into Rating(moodysRating, sandPRating, fitchRating, orderNumber) values ('CCC', 'CC+', 'CC-', 3);
