7034cbba82f0d748f33bbef9d44e68cdced6dce9
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-11 23:10:04 +0200

    Work on PoseidonApplication class

--- src/main/java/com/nnk/springboot/PoseidonApplication.java
+++ src/main/java/com/nnk/springboot/PoseidonApplication.java
@@ -2,8 +2,9 @@ package com.nnk.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class PoseidonApplication {
 
     public static void main(String[] args) {
