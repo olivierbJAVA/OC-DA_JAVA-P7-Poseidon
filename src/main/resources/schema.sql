89eb2e134529a4bdca84c3c0aa72dc0b37a1ed63
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-13 01:39:52 +0200

    Updating schema.sql initialization database file

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -57,7 +57,7 @@ CREATE TABLE Trade (
 
 CREATE TABLE CurvePoint (
   Id tinyint(4) NOT NULL AUTO_INCREMENT,
-  CurveId tinyint,
+  curveId tinyint,
   asOfDate TIMESTAMP,
   term DOUBLE ,
   value DOUBLE ,
@@ -104,3 +104,11 @@ insert into Users(fullname, username, password, role) values("User", "user", "$2
 insert into Rating(moodysRating, sandPRating, fitchRating, orderNumber) values ('AAA', 'AA+', 'AA-', 1);
 insert into Rating(moodysRating, sandPRating, fitchRating, orderNumber) values ('BBB', 'BB+', 'BB-', 2);
 insert into Rating(moodysRating, sandPRating, fitchRating, orderNumber) values ('CCC', 'CC+', 'CC-', 3);
+
+insert into RuleName(name, description, json, template, sqlStr, sqlPart) values ('name1', 'description1', 'json1', 'template1', 'sqlStr1', 'sqlPart1');
+insert into RuleName(name, description, json, template, sqlStr, sqlPart) values ('name2', 'description2', 'json2', 'template2', 'sqlStr2', 'sqlPart2');
+insert into RuleName(name, description, json, template, sqlStr, sqlPart) values ('name3', 'description3', 'json3', 'template3', 'sqlStr3', 'sqlPart3');
+
+insert into CurvePoint(curveId, asOfDate, term, value, creationDate) values (1, '2020-08-12 15:30' , 123.12, 456.45, '2020-08-12 15:30' );
+insert into CurvePoint(curveId, asOfDate, term, value, creationDate) values (2, '2020-08-12 15:30' , 123.12, 456.45, '2020-08-12 15:30' );
+insert into CurvePoint(curveId, asOfDate, term, value, creationDate) values (3, '2020-08-12 15:30' , 123.12, 456.45, '2020-08-12 15:30' );
