c45ec244fe87135e1f90dcea7e78f3049ae5a918
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-18 01:54:49 +0200

    Work on Configuration

--- src/main/java/com/nnk/springboot/configurations/SpringSecurityConfig.java
+++ src/main/java/com/nnk/springboot/configurations/SpringSecurityConfig.java
@@ -3,34 +3,48 @@ package com.nnk.springboot.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
+//@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/user/*", "/admin/home").hasRole("ADMIN")
-                .antMatchers("/", "/bidList/*", "/curvePoint/*", "/rating/*", "/ruleName/*","/trade/*").hasRole("USER")
+                .antMatchers("/user/**", "/admin/home").hasRole("ADMIN")
+                .antMatchers("/", "/bidList/**", "/curvePoint/**", "/rating/**", "/ruleName/**","/trade/**").hasAnyRole("USER","ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
+            .formLogin()
+                //.defaultSuccessUrl("/index.html")
                 .and()
-                .exceptionHandling().accessDeniedPage("/403");
+            .exceptionHandling().accessDeniedPage("/403");
     }
-
+/*
     protected void configure (AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("user").password(passwordEncoder().encode("user")).roles("USER")
                 .and()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN", "USER");
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+                //.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN", "USER");
     }
+*/
+protected void configure (AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
+    auth.jdbcAuthentication()
+        .passwordEncoder(passwordEncoder())
+        .dataSource(dataSource)
+        .usersByUsernameQuery("SELECT username AS principal, password AS credentials, true FROM users WHERE username = ?")
+        .authoritiesByUsernameQuery("SELECT username AS principal, role AS role FROM users WHERE username = ?");
+}
 
     @Bean
     public PasswordEncoder passwordEncoder() {
