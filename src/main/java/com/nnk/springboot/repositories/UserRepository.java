d32a96713fac8511e036a73c38c2554c23db4816
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-19 18:02:09 +0200

    Work on Repository layer

--- src/main/java/com/nnk/springboot/repositories/UserRepository.java
+++ src/main/java/com/nnk/springboot/repositories/UserRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
-
+    User findUserByUsername(String userName);
 }
