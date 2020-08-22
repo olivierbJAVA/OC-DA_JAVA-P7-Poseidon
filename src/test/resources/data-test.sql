04296d7d7cc673d588851d01213f221338aa1647
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-23 00:32:17 +0200

    Updating data-test.sql db init file for test profile

--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,2 +1,2 @@
-insert into Users(fullname, username, password, role) values("Administrator", "admin", "$2a$10$C9hQ7E8sKlJoLJFuS8l.sOwf2hbVprgNUSk4U.X9twlgmnJLNEHwW", "ADMIN");
-insert into Users(fullname, username, password, role) values("Utilisateur", "utilisateur", "$2a$10$dCTx1XYDGXp2AcZrWjrmJecz1EJRYDElZ0VNanGzR/6LCbSkAJCwC", "USER");
+insert into Users(fullname, username, password, role) values('Administrator', 'admin', '$2a$10$yItPXT0kaTk/7gY6yAA7keljOdmQvXwg9.pZNSfOqq5zocpt2z36G', 'ADMIN');
+insert into Users(fullname, username, password, role) values('User', 'user', '$2a$10$qKBKRgXL2fS63N5pvFlL.O5uSzV2A/9EPD.EwGz3M0BfxcF18dBEm', 'USER');
