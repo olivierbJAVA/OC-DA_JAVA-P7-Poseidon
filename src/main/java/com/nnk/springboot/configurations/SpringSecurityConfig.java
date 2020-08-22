03ef1d5754755874ebc278985a91e362ba75d1e3
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-22 17:47:46 +0200

    Work on application security configuration

--- src/main/java/com/nnk/springboot/configurations/SpringSecurityConfig.java
+++ src/main/java/com/nnk/springboot/configurations/SpringSecurityConfig.java
@@ -1,27 +1,35 @@
 package com.nnk.springboot.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-             //.csrf().disable()
              .authorizeRequests()
-                .antMatchers("/user/**", "/admin/home").hasRole("ADMIN")
-                .antMatchers("/", "/bidList/**", "/curvePoint/**", "/rating/**", "/ruleName/**","/trade/**").hasAnyRole("USER","ADMIN")
+                .antMatchers("/user/**", "/admin/home").hasAuthority("ADMIN")
+                .antMatchers("/", "/bidList/**", "/curvePoint/**", "/rating/**", "/ruleName/**","/trade/**").hasAnyAuthority("USER","ADMIN")
+                //.antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -31,23 +39,13 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure (AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password(passwordEncoder().encode("user")).roles("USER")
-                .and()
-                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
-                //.withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN", "USER");
+          auth.jdbcAuthentication()
+            .passwordEncoder(passwordEncoder())
+            .dataSource(dataSource)
+            .usersByUsernameQuery("SELECT username AS principal, password AS credentials, true FROM users WHERE username = ?")
+            .authoritiesByUsernameQuery("SELECT username AS principal, role AS role FROM users WHERE username = ?");
     }
-/*
-protected void configure (AuthenticationManagerBuilder auth, DataSource dataSource) throws Exception {
-    auth.jdbcAuthentication()
-        .passwordEncoder(passwordEncoder())
-        .dataSource(dataSource)
-        .usersByUsernameQuery("SELECT username AS principal, password AS credentials, true FROM users WHERE username = ?")
-        .authoritiesByUsernameQuery("SELECT username AS principal, role AS role FROM users WHERE username = ?");
-        //.usersByUsernameQuery("SELECT username, password, true FROM users WHERE username = ?")
-        //.authoritiesByUsernameQuery("SELECT username, role FROM users WHERE username = ?");
-}
-*/
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
