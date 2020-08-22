72369ceec7f18c9cb4593678f17a9806d91fda7b
olivier.bonheur <olivier.bonheur@gmail.com>
2020-08-23 00:31:30 +0200

    Updating data-dev.sql db init file for development profile

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -1,5 +1,5 @@
-insert into Users(fullname, username, password, role) values('Administrator', 'admin', '$2a$10$pf0c3EX5/UqBLVZsj5BjoufMz1Aa03EHrbJaSci/1eWSkM7Zk7nNm', 'ADMIN');
-insert into Users(fullname, username, password, role) values('Utilisateur', 'utilisateur', '$2a$10$pf0c3EX5/UqBLVZsj5BjoufMz1Aa03EHrbJaSci/1eWSkM7Zk7nNm', 'USER');
+insert into Users(fullname, username, password, role) values('Administrator', 'admin', '$2a$10$yItPXT0kaTk/7gY6yAA7keljOdmQvXwg9.pZNSfOqq5zocpt2z36G', 'ADMIN');
+insert into Users(fullname, username, password, role) values('User', 'user', '$2a$10$qKBKRgXL2fS63N5pvFlL.O5uSzV2A/9EPD.EwGz3M0BfxcF18dBEm', 'USER');
 
 insert into Rating(moodysRating, sandPRating, fitchRating, orderNumber) values ('AAA', 'AA+', 'AA-', 1);
 insert into Rating(moodysRating, sandPRating, fitchRating, orderNumber) values ('BBB', 'BB+', 'BB-', 2);
